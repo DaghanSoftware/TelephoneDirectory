67c4cfc7e7c5a805879389fb82a94388ac78fe1f
Semih Dağhan <daghanex@hotmail.com>
2022-12-14 17:37:57 +0300

    Repository-Layer AppDbContext eklenedi

---
+++ TelephoneDirectoryApp/TelephoneDirectory.Repository/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TelephoneDirectory.Core.Models.Entities;
+
+namespace TelephoneDirectory.Repository
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}
