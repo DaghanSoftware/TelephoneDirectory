dcdc31f2242c69b84e41e7db3a2199f8b50a7dda
Semih Dağhan <daghanex@hotmail.com>
2022-12-15 16:45:10 +0300

    WebAPI- ProductController Methodların Oluşturulması
    -return Ok( CustomResponseDto<List<ProductDto>>.Success(200,productsDtos)); dönüş değerinde zaten status code'u yazmamıza rağmen return Ok diye tekrar belirtmek Best Practices olmayacağı için CustomBaseController isimli ApiController oluşturarak hem kod tekarını önledim hemde daha anlaşılır oldu.

---
+++ TelephoneDirectoryApp/TelephoneDirectory.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TelephoneDirectory.Core.Models.DTOs;
+using TelephoneDirectory.Core.Models.Entities;
+using TelephoneDirectory.Core.Services;
+
+namespace TelephoneDirectory.WebAPI.Controllers
+{
+    public class ProductsController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+
+        public ProductsController(IMapper mapper, IService<Product> service)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _service.GetAllAsync();
+            var productsDtos = _mapper.Map<List<ProductDto>>(products.ToList());
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
+            //return Ok( CustomResponseDto<List<ProductDto>>.Success(200,productsDtos));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var products = await _service.GetByIdAsync(id);
+            var productsDtos = _mapper.Map<ProductDto>(products);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(200, productsDtos));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            var products = await _service.AddAsync(_mapper.Map<Product>(productDto));
+            var productsDtos = _mapper.Map<ProductDto>(products);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(201, productsDtos));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductUpdateDto productDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Product>(productDto));
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Remove(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            await _service.RemoveAsync(product);
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(200));
+        }
+
+    }
+}
