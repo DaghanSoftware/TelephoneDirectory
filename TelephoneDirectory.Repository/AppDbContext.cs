893f2515f89242472cd63d95862cb5cc261ca4c1
Semih Dağhan <daghanex@hotmail.com>
2022-12-18 22:27:09 +0300

    CreateDate/UpdateDate EfCore savechange i çağırana kadar tüm entityleri memory'de track ediyor. Veritabanına yansıtılmadan önce entitynin update mi yoksa inster mi edildiğini anlayan bir middleware yazıp ona göre createdate ve updatedate tarihlerini otomatik olarak veritabanına ekledik

--- TelephoneDirectory.Repository/AppDbContext.cs
+++ TelephoneDirectory.Repository/AppDbContext.cs
@@ -14,6 +14,56 @@ namespace TelephoneDirectory.Repository
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
 
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreateDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                Entry(entityReference).Property(x => x.CreateDate).IsModified = false;
+                                entityReference.UpdatedDate = DateTime.Now;
+                                break;
+                            }
+                    }
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        public override int SaveChanges()
+        {
+            foreach (var item in ChangeTracker.Entries())
+            {
+                if (item.Entity is BaseEntity entityReference)
+                {
+                    switch (item.State)
+                    {
+                        case EntityState.Added:
+                            {
+                                entityReference.CreateDate = DateTime.Now;
+                                break;
+                            }
+                        case EntityState.Modified:
+                            {
+                                Entry(entityReference).Property(x => x.CreateDate).IsModified = false;
+                                entityReference.UpdatedDate = DateTime.Now;
+                                break;
+                            }
+                    }
+                }
+            }
+            return base.SaveChanges();
+        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             #region EntityConfiguration
