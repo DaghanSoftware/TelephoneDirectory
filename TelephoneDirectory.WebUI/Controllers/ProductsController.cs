f28a1a103572254301bbfe61f19c8bee9e9b799d
Semih Dağhan <daghanex@hotmail.com>
2022-12-18 21:37:43 +0300

    Product Controller Update opeasyonları

--- TelephoneDirectory.WebUI/Controllers/ProductsController.cs
+++ TelephoneDirectory.WebUI/Controllers/ProductsController.cs
@@ -48,5 +48,28 @@ namespace TelephoneDirectory.WebUI.Controllers
             ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
             return View();
         }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var products = await _productService.GetByIdAsync(id);
+
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name",products.CategoryId);
+            return View(_mapper.Map<ProductDto>(products));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Update(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productService.UpdateAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name", productDto.CategoryId);
+            return View(productDto);
+        }
     }
 }
