7ddf765034e7e5bf1b16ec73799963210e3b0081
Semih Dağhan <daghanex@hotmail.com>
2022-12-18 20:45:21 +0300

    React arayüzü oluşturulana kadar  MVC projesi oluşturuldu
    Database yolu ayarlandı.
    AutoFac ayarlandı.
    Products Controller oluşturuldu
    Ürünleri listeleme işlemleri yapıldı.
    Ürün kaydetme methodu oluşturuldu.

---
+++ TelephoneDirectory.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TelephoneDirectory.Core.Models.DTOs;
+using TelephoneDirectory.Core.Models.Entities;
+using TelephoneDirectory.Core.Services;
+
+namespace TelephoneDirectory.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
+
+        public ProductsController(IProductService productService, ICategoryService categoryService, IMapper mapper)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var CustomRespone = await _productService.GetProductsWithCategory();
+            return View(CustomRespone.Data);
+        }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories);
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+
+            if (ModelState.IsValid)
+            {
+                await _productService.AddAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+            var categories = _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories);
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+            return View();
+        }
+    }
+}
