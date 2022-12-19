184ca3cfa6b96bcf1fe3f7b0f19624137db7d4af
Semih Dağhan <daghanex@hotmail.com>
2022-12-19 21:28:48 +0300

    ProductApiService Methods

--- TelephoneDirectory.WebUI/Services/ProductApiService.cs
+++ TelephoneDirectory.WebUI/Services/ProductApiService.cs
@@ -1,4 +1,6 @@
-﻿namespace TelephoneDirectory.WebUI.Services
+﻿using TelephoneDirectory.Core.Models.DTOs;
+
+namespace TelephoneDirectory.WebUI.Services
 {
     public class ProductApiService
     {
@@ -7,6 +9,43 @@
         public ProductApiService(HttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
+
+        public async Task<List<ProductWithCategoryDto>> GetProductWithCategoryAsync()
+        {
+            var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<List<ProductWithCategoryDto>>>("product/GetProductWithCategory");
+            return response.Data;
+        }
+
+        public async Task<ProductDto> SaveAsync(ProductDto newproduct)
+        {
+            var response = await _httpClient.PostAsJsonAsync("product", newproduct);
+            if (!response.IsSuccessStatusCode)
+            {
+                return null;
+            }
+            var responseBody = await response.Content.ReadFromJsonAsync<CustomResponseDto<ProductDto>>();
+            return responseBody.Data;
+        }
+
+        public async Task<ProductDto> GetByIdAsync(int id)
+        {
+            var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<ProductDto>>($"product/{id}");
+            return response.Data;
+        }
+
+        public async Task<bool> UpdateAsync(ProductDto newproduct)
+        {
+            var response = await _httpClient.PutAsJsonAsync("product", newproduct);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> RemoveAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"product/{id}");
+
+            return response.IsSuccessStatusCode;
         }
     }
 }
