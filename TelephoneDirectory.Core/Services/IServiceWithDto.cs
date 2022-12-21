6867bb3ec68f6d80e8d8375872debc81c3a1172d
Semih Dağhan <daghanex@hotmail.com>
2022-12-21 20:45:29 +0300

    Generic Service Layer (DTO) Refactoring Custom olan servicelerden dto döndüğümüz gibi aynı şekilde Generic Service içinde Dto dönmesi için Refactorin yapmak gerekiyor. Best practices bir yapı kurmak için   Generic servisten dto dönmek önemlidir.

---
+++ TelephoneDirectory.Core/Services/IServiceWithDto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TelephoneDirectory.Core.Models.DTOs;
+using TelephoneDirectory.Core.Models.Entities;
+
+namespace TelephoneDirectory.Core.Services
+{
+    public interface IServiceWithDto<Entity,Dto> where Entity:BaseEntity where Dto:class
+    {
+        Task<CustomResponseDto<Dto>> GetByIdAsync(int id);
+
+        Task<IEnumerable<CustomResponseDto<Dto>>> GetAllAsync();
+
+        Task<IEnumerable<CustomResponseDto<Dto>>> Where(Expression<Func<Entity, bool>> expression);
+
+        Task<CustomResponseDto<bool>> AnyAsync(Expression<Func<Entity, bool>> expression);
+
+        Task<CustomResponseDto<Dto>> AddAsync(Dto dto);
+
+        Task<CustomResponseDto<IEnumerable<Dto>>> AddRangeAsync(IEnumerable<Dto> dto);
+
+        Task<CustomResponseDto<NoContentDto>> UpdateAsync(Dto dto);
+
+        Task<CustomResponseDto<NoContentDto>> RemoveAsync(int id);
+
+        Task<CustomResponseDto<NoContentDto>> RemoveRangeAsync(IEnumerable<int> ids);
+    }
+}
