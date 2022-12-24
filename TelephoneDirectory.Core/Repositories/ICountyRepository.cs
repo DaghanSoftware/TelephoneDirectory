efd39baaaf1c18095cba8ff7787c5d9822db072f
Semih Dağhan <daghanex@hotmail.com>
2022-12-24 17:46:22 +0300

    Repository'ler oluşturuldu.

---
+++ TelephoneDirectory.Core/Repositories/ICountyRepository.cs
@@ -0,0 +1,8 @@
+﻿using TelephoneDirectory.Core.Models.Entities;
+
+namespace TelephoneDirectory.Core.Repositories
+{
+    public interface ICountyRepository : IGenericRepository<County>
+    {
+    }
+}
