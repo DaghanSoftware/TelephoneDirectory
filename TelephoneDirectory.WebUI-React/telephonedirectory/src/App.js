dd150b41b744334e6877364cc2724672e2d7d258
Semih Dağhan <daghanex@hotmail.com>
2022-12-27 23:45:29 +0300

    Tasarımdaki hataların tespiti

--- TelephoneDirectory.WebUI-React/telephonedirectory/src/App.js
+++ TelephoneDirectory.WebUI-React/telephonedirectory/src/App.js
@@ -1,12 +1,25 @@
-import Home from './components/Home';
-import './assets/css/index.css';
-import './assets/css/animated.css';
+import Home from "./components/Home";
+// import './vendor/bootstrap/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/fontawesome.css';
-import './assets/css/owl.css';
+import './assets/css/templatemo-plot-listing.css';
+import './assets/css/animated.css';
+// import './assets/css/owl.css';
+
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
+import './assets/js/owl-carousel';
+import './assets/js/animation';
+import './assets/js/imagesloaded';
+import './assets/js/custom';
+
+
 function App() {
   return (
     <div className="App">
+      <h1>
         <Home></Home>
+      </h1>
     </div>
   );
 }
