238d8cad9ab93986e9210d5bc999ce9ab57e35d9
Semih Dağhan <daghanex@hotmail.com>
2022-12-28 23:47:16 +0300

    Template tamamen kaldırıldı. Yeni template ayarlanacak

--- TelephoneDirectory.WebUI-React/telephonedirectory/src/App.js
+++ TelephoneDirectory.WebUI-React/telephonedirectory/src/App.js
@@ -1,25 +1,9 @@
 import Home from "./components/Home";
-// import './vendor/bootstrap/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './assets/css/fontawesome.css';
-import './assets/css/templatemo-plot-listing.css';
-import './assets/css/animated.css';
-// import './assets/css/owl.css';
-
-import '../node_modules/jquery/dist/jquery.min.js';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
-import './assets/js/owl-carousel';
-import './assets/js/animation';
-import './assets/js/imagesloaded';
-import './assets/js/custom';
-
 
 function App() {
   return (
     <div className="App">
-      <h1>
         <Home></Home>
-      </h1>
     </div>
   );
 }
