b2ed13a0425060a8bce41fde3d2d234868198015
Semih Dağhan <daghanex@hotmail.com>
2023-01-01 21:45:56 +0300

    Template oluşturuldu.

--- TelephoneDirectory.WebUI-React/telephonedirectory/src/App.js
+++ TelephoneDirectory.WebUI-React/telephonedirectory/src/App.js
@@ -1,4 +1,18 @@
 import Home from "./components/Home";
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './assest/font-awesome-4.7.0/css/font-awesome.min.css';
+import './assest/css/bootstrap.min.css';
+import './assest/css/datepicker.css';
+import './assest/css/tooplate-style.css';
+import './assest/slick/slick.css';
+import './assest/slick/slick-theme.css';
+
+// import './js/jquery-1.11.3.min';
+// import './js/popper.min.js';
+// import './js/bootstrap.min.js';
+// import './js/datepicker.min.js';
+// import './js/jquery-1.11.3.min';
+// import './slick/slick.min';
 
 function App() {
   return (
--- TelephoneDirectory.WebUI-React/telephonedirectory/src/components/Menu.js
+++ TelephoneDirectory.WebUI-React/telephonedirectory/src/components/Menu.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import React from "react";
 
-export const Menu = () => {
+const Menu = () => {
   return (
-    <div>Menu</div>
-  )
-}
+    <div className="tm-top-bar" id="tm-top-bar">
+      {/* <div className="tm-top-bar-bg"></div> */}
+      <div className="container">
+        <div className="row">
+          <nav className="navbar navbar-expand-lg narbar-light">
+            <a className="navbar-brand mr-auto" href="#">
+              <img src="img/logo.png" alt="Site logo"></img>
+              Level
+            </a>
+            <button
+              type="button"
+              id="nav-toggle"
+              className="navbar-toggler collapsed"
+              data-toggle="collapse"
+              data-target="#mainNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div id="mainNav" className="collapse navbar-collapse tm-bg-white">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a className="nav-link" href="#top">
+                    Anasayfa <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#tm-section-4">
+                    Portfolio
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#tm-section-5">
+                    Blog Entries
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#tm-section-6">
+                    Contact Us
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Menu;
